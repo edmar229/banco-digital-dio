bfe0325db1741141f421720a68eda422b9dca13b
Edmar <edmarvieira003@gmail.com>
2024-06-25 09:21:11 -0300

    base project

---
+++ src/Main.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("BEM VINDO AO BANCO DIGITAL\n 1.Entrar \n 2.Criar conta corrente\n 3.Criar conta poupança\n 4.Sair\n");
+
+		String decisao = scan.nextLine();
+
+		switch (decisao) {
+			case "1":
+				System.out.println("Login:");
+				break;
+			case "2":
+				System.out.println("INSIRA OS DADOS:");
+				break;
+			case "3":
+				System.out.println("INSIRA OS DADOS:");
+				break;
+			case "4":
+				System.out.println("Saindo...");
+			default:
+				System.out.println("Opção inválida");
+				break;
+		}
+
+		scan.close();
+	}
+
+}
