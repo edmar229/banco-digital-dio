06b7fb9f6a4512584f88edb43aa6ada554a5f744
Edmar <edmarvieira003@gmail.com>
2024-06-28 08:49:32 -0300

    menu opcoes loop ok

--- src/Main.java
+++ src/Main.java
@@ -8,27 +8,32 @@ public class Main {
 		Scanner scan = new Scanner(System.in);
 
 		Banco banco = new Banco();
+
+		int opcao = 0;
 		
-		System.out.println("BEM VINDO AO BANCO DIGITAL\n 1.Entrar \n 2.Criar conta corrente\n 3.Criar conta poupança\n 4.Sair\n");
+		do {
+			System.out.println("BEM VINDO AO BANCO DIGITAL\n 1.Entrar \n 2.Criar conta corrente\n 3.Criar conta poupança\n 4.Sair");
+			System.out.println("Escolha a opção: ");
 
-		String decisao = scan.nextLine();
+			opcao = scan.nextInt();
 
-		switch (decisao) {
-			case "1":
-				System.out.println("Login:");
-				break;
-			case "2":
-				System.out.println("INSIRA OS DADOS:");
-				break;
-			case "3":
-				System.out.println("INSIRA OS DADOS:");
-				break;
-			case "4":
-				System.out.println("Saindo...");
-			default:
-				System.out.println("Opção inválida");
-				break;
-		}
+			switch (opcao) {
+				case 1:
+					System.out.println("Login:");
+					break;
+				case 2:
+					System.out.println("INSIRA OS DADOS:");
+					break;
+				case 3:
+					System.out.println("INSIRA OS DADOS:");
+					break;
+				case 4:
+					System.out.println("Saindo...");
+				default:
+					System.out.println("Opção inválida");
+					break;
+			}
+		} while (opcao != 4);
 
 		scan.close();
 	}
