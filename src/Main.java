a5efc7db260050e4abf4149bbb764ca9dee84894
Edmar <edmarvieira003@gmail.com>
2024-06-28 09:44:35 -0300

    finalizando desafio

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 import banco.Banco;
+import cadastro.CriarConta;
+import cadastro.EnumTipoConta;
 
 public class Main {
 
@@ -8,32 +11,34 @@ public class Main {
 		Scanner scan = new Scanner(System.in);
 
 		Banco banco = new Banco();
+		banco.setContas(new ArrayList<>());
 
 		int opcao = 0;
 		
 		do {
-			System.out.println("BEM VINDO AO BANCO DIGITAL\n 1.Entrar \n 2.Criar conta corrente\n 3.Criar conta poupança\n 4.Sair");
+			System.out.println("BEM VINDO AO BANCO DIGITAL\n 1.Criar conta corrente\n 2.Criar conta poupança\n 3.Sair");
 			System.out.println("Escolha a opção: ");
 
 			opcao = scan.nextInt();
 
 			switch (opcao) {
 				case 1:
-					System.out.println("Login:");
+					System.out.println("INSIRA OS DADOS REQUERIDOS: ");
+					CriarConta criadorContaCorrente = new CriarConta();
+					banco.getContas().add(criadorContaCorrente.cadastrarCliente(EnumTipoConta.CORRENTE));
 					break;
 				case 2:
-					System.out.println("INSIRA OS DADOS:");
+					System.out.println("INSIRA OS DADOS REQUERIDOS: ");
+					CriarConta criadorContaPoupanca = new CriarConta();
+					banco.getContas().add(criadorContaPoupanca.cadastrarCliente(EnumTipoConta.CORRENTE));
 					break;
 				case 3:
-					System.out.println("INSIRA OS DADOS:");
-					break;
-				case 4:
 					System.out.println("Saindo...");
-				default:
-					System.out.println("Opção inválida");
 					break;
+				default:
+					System.out.println("Opção inválida!");
 			}
-		} while (opcao != 4);
+		} while (opcao != 3);
 
 		scan.close();
 	}
--- src/cadastro/CriarConta.java
+++ src/cadastro/CriarConta.java
@@ -8,19 +8,19 @@ import banco.ContaCorrente;
 import banco.ContaPoupanca;
 
 public class CriarConta {
+  
   public Conta cadastrarCliente(EnumTipoConta tipoConta) {
-    Scanner scan = new Scanner(System.in);
+    System.out.println("------CADASTRO EM ANDAMENTO------");
+    Scanner scanner = new Scanner(System.in);
 
     System.out.println("Digite seu nome: ");
-    String nome = scan.nextLine();
+    String nome = scanner.nextLine();
     System.out.println("Digite seu cpf: ");
-    int cpf = scan.nextInt();
+    int cpf = scanner.nextInt();
 
     Cliente cliente = new Cliente();
     cliente.setNome(nome);
     cliente.setCpf(cpf);
-
-    scan.close();
 
     if (tipoConta.equals(EnumTipoConta.CORRENTE)) {
       ContaCorrente contaCorrente = new ContaCorrente(cliente);
